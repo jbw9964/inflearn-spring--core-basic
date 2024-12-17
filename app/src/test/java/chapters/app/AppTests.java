7911cf12e1ee753360df3720488791a711c2d7a7
청주는사과아님 <jbw9964@gmail.com>
2024-12-17 17:13:44 +0900

    init: 프로젝트 init

---
+++ app/src/test/java/chapters/app/AppTests.java
@@ -0,0 +1,13 @@
+package chapters.app;
+
+import org.junit.jupiter.api.*;
+import org.springframework.boot.test.context.*;
+
+@SpringBootTest
+class AppTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
